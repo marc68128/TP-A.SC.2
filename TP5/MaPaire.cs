31e970299af5710ce4d3c3de70ba2ab475e19c26
Marc Unterseh <148278@supinfo.com>
2014-12-05 12:47:59 +0100

    TP5

---
+++ TP5/MaPaire.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5
+{
+    public class MaPaire<T, U>
+    {
+        public T Premier { get; set; }
+        public U Second { get; set; }
+
+        public MaPaire(T prem, U sec)
+        {
+            Premier = prem;
+            Second = sec;
+        }
+
+        public MaPaire<U, T> Swap()
+        {
+            return new MaPaire<U, T>(Second, Premier);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Valeur 1 : {0} – Valeur 2 : {1}", Premier, Second);
+        }
+    }
+}
---
+++ TP5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaPaire<string, int> maPaire = new MaPaire<string, int>("Dumez", 42);
+            MaPaire<int, string> maPaireInverse = maPaire.Swap(); 
+
+            Console.WriteLine(maPaire);
+            Console.WriteLine(maPaireInverse);
+
+            MaPaire<string, int>[] mesPaires = new MaPaire<string, int>[3];
+            mesPaires[0] = new MaPaire<string, int>("toto", 22);
+            mesPaires[1] = new MaPaire<string, int>("tutu", 25);
+            mesPaires[2] = new MaPaire<string, int>("tata", 26);
+
+            foreach (var maPaire2 in mesPaires)
+            {
+                Console.WriteLine(maPaire2.ToString());
+            }
+
+            MaPaire<MaPaire<int, int>, string> maPaire3 = new MaPaire<MaPaire<int, int>, string>(new MaPaire<int, int>(10,20), "salut" );
+            Console.WriteLine(maPaire3);
+
+            Console.ReadLine(); 
+        }
+    }
+}
